b34c456299f1e66fc695c058d3f2afedcfde9eb7
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-03-08 13:19:59 -0400

    add management index route

---
+++ app/routes/management/index.tsx
@@ -0,0 +1,10 @@
+import Section from "~/components/section";
+
+export default function ManagementIndexRoute() {
+	return (
+		<Section
+			title="Hola de vuelta!"
+			description="Selecciona alguna opción del menú para acceder a los modulos del sistema"
+		/>
+	);
+}
